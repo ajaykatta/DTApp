255aaab11552d907425a37c5dc9009813793f809
ajaykatta <katta.ajay@live.com>
2020-05-26 21:33:02 -0500

    end of section 8

---
+++ DTApp.API/Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DTApp.API.Modals;
+
+namespace DTApp.API.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+    }
+}
